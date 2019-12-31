cfd74f3813dffcc583dc554a43d1d0dfc580fd74
Khairul Azmi <azmi@getslurp.com>
2019-12-31 09:11:34 +0800

    Updates

---
+++ precache-manifest.80a16cfa6f05979fd1f680d58abec977.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc91d1867dc1db69888177ddee3d4d7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2b89b106ccd5153591b6",
+    "url": "/robofriends/static/css/main.64a77ad3.chunk.css"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/robofriends/static/js/2.8d7c7b1f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.8d7c7b1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b89b106ccd5153591b6",
+    "url": "/robofriends/static/js/main.1bfa70da.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
